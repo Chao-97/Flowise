339af450d8b3c8545bb7d46cd20f32ccee0805ae
lichaowei <13503520128@163.com>
2024-01-31 16:04:41 +0800

    翻译

---
+++ packages/server/ecosystom.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Tensify',
+            interpreter: './node_modules/.bin/ts-node',
+            interpreter_args: '-r tsconfig-paths/register',
+            instance_var: 'INSTANCE_ID',
+            instances: 2,
+            exec_mode: 'cluster',
+            cwd: './',
+            script: '/Users/lichaowei/Jobs/vms/Flowise/packages/server/start.ts'
+        }
+    ]
+}
